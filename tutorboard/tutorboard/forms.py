a35c4abf2ee5a5a8878327100d86e991778e61cd
Thomas Ricard <thomas@codesters.com>
2015-06-16 11:04:54 -0400

    cleaned up broken references. updated forms to 1.8, which requires the fields attribute

--- tutorboard/tutorboard/forms.py
+++ tutorboard/tutorboard/forms.py
@@ -1,19 +1,17 @@
-#forms.py
+# forms.py
 from django import forms
 from django.forms import ModelForm, ChoiceField
 from django.forms.fields import Field
 from tutorboard.models import Tutor, Capability, Subject
 from django.forms import CheckboxSelectMultiple, TextInput, HiddenInput, Textarea
 
-setattr(Field, 'is_hidden', lambda self: isinstance(self.widget, forms.HiddenInput ))
+setattr(Field, 'is_hidden', lambda self: isinstance(self.widget, forms.HiddenInput))
 
 BIRTH_YEAR_CHOICES = ('1980', '1981', '1982')
-FAVORITE_COLORS_CHOICES = (('blue', 'Blue'),
-                            ('green', 'Green'),
-                            ('black', 'Black'))
-AVAILABILITY_CHOICES = [('zero', '0 - 5'),('five','5 - 10'),('ten','10 - 15')]
-LEVEL_CHOICES = [('PR','Professional'),('EX','Expert'),('DR','Director')]
-SUBJECT_CHOICES = [('cornerstone','Cornerstone'),('echelon','Echelon'),('sat','SAT'),('academic','Academic')]
+FAVORITE_COLORS_CHOICES = (('blue', 'Blue'), ('green', 'Green'), ('black', 'Black'))
+AVAILABILITY_CHOICES = [('zero', '0 - 5'), ('five', '5 - 10'), ('ten', '10 - 15')]
+LEVEL_CHOICES = [('PR', 'Professional'), ('EX', 'Expert'), ('DR', 'Director')]
+SUBJECT_CHOICES = [('cornerstone', 'Cornerstone'), ('echelon', 'Echelon'), ('sat', 'SAT'), ('academic', 'Academic')]
 
 
 class SearchForm(forms.Form):
@@ -26,25 +24,25 @@ class SearchForm(forms.Form):
 class TutorForm(forms.ModelForm):
     class Meta:
         model = Tutor
+        fields = ['bioline1',
+                  'bioline2',
+                  'bioline3',
+                  'bioline4',
+                  'bioline5',
+                  'highestLevel',
+                  'availability_note',
+                  'availability_vacation']
         widgets = {
-            'bioline1': TextInput(attrs={'class':'update-bio'}),
-            'bioline2': TextInput(attrs={'class':'update-bio'}),
-            'bioline3': TextInput(attrs={'class':'update-bio'}),
-            'bioline4': TextInput(attrs={'class':'update-bio'}),
-            'bioline5': TextInput(attrs={'class':'update-bio'}),
-            'energy': CheckboxSelectMultiple(attrs={'class':'update-checkbox'}),
-            'presence': CheckboxSelectMultiple(attrs={'class':'update-checkbox'}),
-            'archetype': CheckboxSelectMultiple(attrs={'class':'update-checkbox'}),
-            'location': CheckboxSelectMultiple(attrs={'class':'update-checkbox'}),
-            'styles': CheckboxSelectMultiple(attrs={'class':'update-checkbox'}),
-            'levelPreferences': CheckboxSelectMultiple(attrs={'class':'update-checkbox'}),
-            'studentEngagements': CheckboxSelectMultiple(attrs={'class':'update-checkbox'}),
-            'skills': CheckboxSelectMultiple(attrs={'class':'update-checkbox'}),
+            'bioline1': TextInput(attrs={'class': 'update-bio'}),
+            'bioline2': TextInput(attrs={'class': 'update-bio'}),
+            'bioline3': TextInput(attrs={'class': 'update-bio'}),
+            'bioline4': TextInput(attrs={'class': 'update-bio'}),
+            'bioline5': TextInput(attrs={'class': 'update-bio'}),
             'highestLevel': HiddenInput(),
-            'availability_note':Textarea(attrs={'rows':'4', 'cols':'40'}),
-            'availability_vacation':Textarea(attrs={'rows':'4', 'cols':'40'}),
-            'sex': HiddenInput(),
+            'availability_note': Textarea(attrs={'rows': '4', 'cols': '40'}),
+            'availability_vacation': Textarea(attrs={'rows': '4', 'cols': '40'}),
         }
+
 
 class AvailabilityForm(forms.ModelForm):
     class Meta:
@@ -55,13 +53,14 @@ class AvailabilityForm(forms.ModelForm):
 class SubjectForm(forms.ModelForm):
     class Meta:
         model = Subject
+        fields = ['name', 'image', 'is_ap', 'imageAP']
 
 
 class CapabilityForm(forms.ModelForm):
-     
-    class Meta: 
+    class Meta:
         model = Capability
-        widgets = {            
-            'score': TextInput(attrs={'size':'4'}),
-            'notes': Textarea(attrs={'rows':'4', 'cols':'40'}),
+        fields = ['level', 'level_note', 'score', 'area', 'notes', 'subject', 'tutor']
+        widgets = {
+            'score': TextInput(attrs={'size': '4'}),
+            'notes': Textarea(attrs={'rows': '4', 'cols': '40'}),
         }
